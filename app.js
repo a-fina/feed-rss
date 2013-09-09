c7b3af3ef7a9d0bb0fb6cadc6fbb67f0a890bf34
bayois <bayois@gmail.com>
2013-09-09 16:07:35 +0200

    iModificata porta

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var flatiron = require('flatiron'),
     path = require('path'),
     app = flatiron.app;
 
+var port = process.env.PORT || 5000;
+
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
 app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
+  this.res.json({ 'hello': 'world from flatiron' })
 });
 
-app.start(3000);
+app.start(port);
